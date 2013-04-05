0f56181b2907887ba1ea58503da48edb2c7e7b79
Andrew Ducker <andrew@ducker.org.uk>
2013-04-05 14:49:55 +0100

    Velocity now imported.

---
+++ Postr/src/com/postr/PersonaLogoutServlet.java
@@ -0,0 +1,15 @@
+package com.postr;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PersonaLogoutServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+    	req.getSession().setAttribute("EmailAddress", null);
+	}
+}
