316c3ac2f78f242171005f08862e52fa9f850790
Andrew Ducker <andrew@ducker.org.uk>
2014-08-03 13:19:27 +0100

    Reorganising to make JS concatenation work better.

--- Postr/gulpfile.js
+++ Postr/gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
 concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('controllers', function() {
-      return gulp.src(['Websrc/Controllers/init.js','Websrc/Controllers/*.js'])
+      return gulp.src(['Websrc/js/Controllers/init.js','Websrc/js/Controllers/*.js'])
           .pipe(ngAnnotate())
           .pipe(concat('controllers.js'))
-        .pipe(gulp.dest('war'));
+          .pipe(gulp.dest('build'));
+});
+
+gulp.task('miscjs', function(){
+	return gulp.src(['Websrc/js/misc/*.js'])
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('alljs',['controllers','miscjs'],function(){
+	return gulp.src(['build/*.js'])
+		.pipe(concat('alljs.js'))
+//           .pipe(uglify())
+		.pipe(gulp.dest('war'));
 });
 
 gulp.task('default',function(){
-	return gulp.watch('Websrc/Controllers/*',['controllers']);
+	gulp.watch('Websrc/js/**/*',['alljs']);
 });
