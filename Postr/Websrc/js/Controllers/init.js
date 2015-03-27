6fa91d51b9d2ca852c22d6e1fd695989ffd182c6
Andrew Ducker <andrew@ducker.org.uk>
2015-03-27 23:34:20 +0000

    Now linkifying the returned URLs!

--- Postr/Websrc/js/Controllers/init.js
+++ Postr/Websrc/js/Controllers/init.js
@@ -1,2 +1,2 @@
 //"use strict";
-var postrApp = angular.module('postrApp', [ 'ui.bootstrap','ngRoute', 'multi-select', 'ui.validate','ngMessages' ]); 
+var postrApp = angular.module('postrApp', [ 'ui.bootstrap','ngRoute', 'multi-select', 'ui.validate','ngMessages','ngSanitize' ]); 
