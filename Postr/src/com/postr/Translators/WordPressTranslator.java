1a08032a0a3ae7e808cb2839e50e8ef2e8e4c97c
Andrew Ducker <andrew@ducker.org.uk>
2015-11-13 17:45:22 +0000

    Posting via REST now working.

--- Postr/src/com/postr/Translators/WordPressTranslator.java
+++ Postr/src/com/postr/Translators/WordPressTranslator.java
@@ -1,24 +1,24 @@
 package com.postr.Translators;
 
+import org.apache.commons.lang.StringUtils;
+import org.json.JSONObject;
+
+import com.goebl.david.Webb;
 import com.postr.Result;
 import com.postr.DataTypes.Outputs.WordPressData;
 
 
 public class WordPressTranslator {
-		public Result MakePost(WordPressData data, String contents, String header, String[] tags) throws Exception{
-//			try {
-//			    XmlRpcClient client = getClient();
-//				HashMap<String,Object> postParams = new HashMap<String,Object>();
-//				postParams.put("title", header);
-//				postParams.put("description",contents);
-//				postParams.put("mt_keywords",tags);
-//				Object[] params = new Object[]{"NotUsedByWordPress",data.userName,data.GetDecryptedPassword(),postParams,true};
-//				String ret = (String) client.execute("metaWeblog.newPost", params);
-//
-//				URL resultUrl = new URL(new URL(serverURL),"?p="+ret); 
-				String resultUrl="Fake!";
-				return Result.Success(resultUrl.toString());
-//			}
-			}
+	public Result MakePost(WordPressData data, String contents, String header, String[] tags) throws Exception{
+		Webb webb = Webb.create();
+		JSONObject result = webb.post("https://public-api.wordpress.com/rest/v1.1/sites/" + data.blogId + "/posts/new")
+				.param("title", header)
+				.param("content", contents)
+				.param("tags", StringUtils.join(tags,","))
+				.header("Authorization", "Bearer "+data.password)
+				.asJsonObject().getBody();
 
+		String resultUrl=result.getString("URL");
+		return Result.Success(resultUrl.toString());
+	}
 }
